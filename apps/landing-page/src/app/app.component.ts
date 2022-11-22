f1f99645fb47847020ae1d896e93ac6d01b8700a
lemmy mwaura <lemminton@gmail.com>
2022-11-22 18:49:53 +0300

    Initial commit

---
+++ apps/landing-page/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pivo-test-workspace-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'landing-page';
+}
