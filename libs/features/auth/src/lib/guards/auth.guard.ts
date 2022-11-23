a7fd77a4faac4c47106173a93f91303924916a17
lemmy mwaura <lemminton@gmail.com>
2022-11-23 22:08:25 +0300

    feat: add create account functionality

--- libs/features/auth/src/lib/guards/auth.guard.ts
+++ libs/features/auth/src/lib/guards/auth.guard.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '../providers/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class IsLoggedIn implements CanActivate {
-  constructor(private _authService: AuthService) {}
+  constructor(private _authService: AuthService, private _router: Router) {}
 
   canActivate() {
-    return true;
+    if (this._authService.isLoggedIn()) {
+      return true;
+    } else {
+      this._router.navigate(['/auth/login']);
+      return false;
+    }
   }
 }
--- libs/features/auth/src/lib/providers/auth.service.ts
+++ libs/features/auth/src/lib/providers/auth.service.ts
@@ -1,17 +1,42 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+export interface User {
+  uid: string;
+  email: string;
+  displayName?: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(
+    private _afAuth: AngularFireAuth,
+    private _afs: AngularFirestore
+  ) {}
 
-  constructor() { "" }
-
-  isLoggedIn(){
-    ""
+  public isLoggedIn() {
+    return true
   }
 
-  createAccountWithEmailAndPassword() {
-    ""
+  public createUserWithEmailAndPassword(
+    displayName: string,
+    email: string,
+    password: string
+  ) {
+    return this._afAuth
+      .createUserWithEmailAndPassword(email, password)
+      .then((res) => {
+        return res.user;
+      })
+      .catch((error) => {
+        this._throwError(error);
+      });
+  }
+
+  private _throwError(error: any) {
+    console.log(error.message);
   }
 }
