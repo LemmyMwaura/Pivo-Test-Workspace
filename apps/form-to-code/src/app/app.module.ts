4e2a1c8a114dbcd30c4ed95a276f6b087ed75f58
lemmy mwaura <lemminton@gmail.com>
2022-11-23 16:58:05 +0300

    feat: setup firebase

--- apps/form-to-code/src/app/app.module.ts
+++ apps/form-to-code/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { PreloadAllModules, RouterModule } from '@angular/router';
-import { appRoutes } from './app.routes';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
+import { PreloadAllModules, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    
     RouterModule.forRoot(appRoutes, {
       initialNavigation: 'enabledBlocking',
       enableTracing: true,
