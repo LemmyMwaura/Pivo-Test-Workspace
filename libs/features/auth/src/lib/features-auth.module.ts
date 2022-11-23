98a7ab21e62b638fc9acff38401c43219bde03c1
lemmy mwaura <lemminton@gmail.com>
2022-11-23 15:35:51 +0300

    feat: export authguards

--- libs/features/auth/src/lib/features-auth.module.ts
+++ libs/features/auth/src/lib/features-auth.module.ts
@@ -1,5 +1,11 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+//guards & services
+import { AuthService } from './providers/auth.service';
+import { AuthGuard } from './guards/auth.guard';
+
+//components
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
@@ -7,4 +13,11 @@ import { RegisterComponent } from './components/register/register.component';
   imports: [CommonModule],
   declarations: [LoginComponent, RegisterComponent],
 })
-export class FeaturesAuthModule {}
+export class FeaturesAuthModule {
+  static forRoot(): ModuleWithProviders<FeaturesAuthModule> {
+    return {
+      ngModule: FeaturesAuthModule,
+      providers: [AuthGuard, AuthService],
+    };
+  }
+}
