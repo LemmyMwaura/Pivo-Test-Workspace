6d3a26772847042fe913d3292b2c44b96a218231
lemmy mwaura <lemminton@gmail.com>
2022-11-23 13:19:49 +0300

    feat: add home page to form app

--- libs/elements/form/home/src/lib/elements-form-home.module.ts
+++ libs/elements/form/home/src/lib/elements-form-home.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [HomeComponent],
 })
 export class ElementsFormHomeModule {}
---
+++ libs/elements/form/home/src/lib/pages/home/home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pivo-test-workspace-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {}
