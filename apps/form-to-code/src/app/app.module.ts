362ce35eb5c173244472a36e481aec105968cc1c
lemmy mwaura <lemminton@gmail.com>
2022-11-23 15:36:08 +0300

    feat: setup default routes

--- apps/form-to-code/src/app/app.module.ts
+++ apps/form-to-code/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
+import { PreloadAllModules, RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    RouterModule.forRoot(appRoutes, {
+      initialNavigation: 'enabledBlocking',
+      enableTracing: true,
+      preloadingStrategy: PreloadAllModules,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- apps/form-to-code/src/app/app.routes.ts
+++ apps/form-to-code/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Route } from '@angular/router';
+import { AuthGuard } from '@pivo-test-workspace/features/auth';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('@pivo-test-workspace/elements/form/home').then(
+        (m) => m.ElementsFormHomeModule
+      ),
+    canActivate: [AuthGuard],
+  },
+
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('@pivo-test-workspace/features/auth').then(
+        (m) => m.FeaturesAuthModule
+      ),
+  },
+];
