3f01d3439e7e5b1128d73d7278738ba353d22208
lemmy mwaura <lemminton@gmail.com>
2022-11-23 12:10:26 +0300

    feat: setup login and register components

---
+++ libs/features/auth/src/lib/components/login/login.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pivo-test-workspace-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {}
