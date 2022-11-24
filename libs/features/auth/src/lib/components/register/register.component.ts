5a227e7112bfb2c70f4565accc128c30089d1cfd
lemmy mwaura <lemminton@gmail.com>
2022-11-24 23:08:19 +0300

    style: add and style reg form

--- libs/features/auth/src/lib/components/register/register.component.ts
+++ libs/features/auth/src/lib/components/register/register.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'pivo-test-workspace-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {}
+export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+  userIcon = faUser;
+  lockIcon = faLock;
+  emailIcon = faEnvelope;
+
+  registerFormGroup() {
+    this.registerForm = this._formBuilder.group({
+      name: ['', Validators.required, Validators.minLength(4)],
+      email: ['', Validators.required],
+      password: ['', Validators.required, Validators.minLength(6)],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.registerForm.value);
+  }
+
+  ngOnInit(): void {
+    this.registerFormGroup();
+  }
+}
