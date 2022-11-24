39f0f17364a8ec2d8a3936c0b381e3d0ebecdd7d
lemmy mwaura <lemminton@gmail.com>
2022-11-24 21:03:15 +0300

    style: add login styles and some functionality

--- libs/features/auth/src/lib/components/login/login.component.ts
+++ libs/features/auth/src/lib/components/login/login.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'pivo-test-workspace-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  accountForm!:FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+  getUser = () => "assets/images/User.png"
+
+  getLock = () => "assets/images/Lock.png"
+
+  loginFormGroup() {
+    this.accountForm = this._formBuilder.group({
+      name: ['', Validators.required, Validators.minLength(4)],
+      password: ['', Validators.required, Validators.minLength(6)]
+    })
+  }
+
+  onSubmit(){
+    console.log(this.accountForm.value)
+  }
+
+  ngOnInit(): void {
+    this.loginFormGroup()
+  }
+}
