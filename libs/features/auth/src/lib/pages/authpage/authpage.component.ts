6ed762bf1bc8dd1a1aefac4ef0a99f47af2a3923
lemmy mwaura <lemminton@gmail.com>
2022-11-24 21:05:01 +0300

    style: update auth styles

--- libs/features/auth/src/lib/pages/authpage/authpage.component.ts
+++ libs/features/auth/src/lib/pages/authpage/authpage.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './authpage.component.html',
   styleUrls: ['./authpage.component.scss'],
 })
-export class AuthpageComponent {}
+export class AuthpageComponent {
+  isLogin = true
+
+  getVector = () => "assets/images/Vector.png"
+  getCart = () => "assets/images/Cart.png"
+  getEllipseSmall = () => "assets/images/Ellipse.png"
+  getEllipseSmaller = () => "assets/images/Ellipse-1.png"
+  getEllipseSmallest = () => "assets/images/Ellipse-2.png"
+}
